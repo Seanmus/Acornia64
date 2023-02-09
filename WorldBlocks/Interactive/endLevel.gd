8c947fb47ebf04950930c61b9d506bc75b438fd2
Sean Piche <spiche91@academic.rrc.ca>
2023-02-09 16:47:37 -0600

    Fixed spacing between platforms on disapearing platform level to make the gameplay more fluid

--- WorldBlocks/Interactive/endLevel.gd
+++ WorldBlocks/Interactive/endLevel.gd
@@ -7,4 +7,5 @@ signal won
 #Triggers the win condition on the player
 func _on_end_level_body_entered(body):
 	if body.is_in_group("Player"):
-		body.Win();
+		Manager.nextLevel = nextLevel
+		body.Win()
