8d941d424aa444ac4e1a26cb47a077514d43ba43
Seanmus <seanpiche718@gmail.com>
2023-06-24 14:57:18 -0500

    Fixed ui overlay for loading

--- Worlds/StartUi.gd
+++ Worlds/StartUi.gd
@@ -1,9 +1,7 @@
 extends Node
 @onready var audioBus := AudioServer.get_bus_index("Master")
 @onready var animationPlayer = $Transition/AnimationPlayer
-var loadLevel
-#@onready var collect_a_thon = load("res://Worlds/collect_athon.tscn")
-#var forest = preload("res://Worlds/new_forest.tscn").instance()
+
 
 func _ready():
 	MusicPlayer.playing = true
@@ -19,13 +17,9 @@ func _process(_delta):
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
 		
-#Plays the transition animation
-func _on_button_pressed():
-	animationPlayer.play("endFast")
-
 #Loads the first level
-func _on_animation_player_animation_finished(_anim_name):
-	var _returnValue = get_tree().change_scene_to_file("res://Worlds/new_forest.tscn")
+func _on_button_pressed():
+	LevelLoader._loadLevel("new_forest")
 
 #Sets the audio bus volume to the sliders value
 func _on_audio_slider_value_changed(value):
