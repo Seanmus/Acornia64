0775456b243bf2f7acb5823ccf6c7b216d4a1dc8
Seanmus <seanpiche718@gmail.com>
2024-10-23 15:56:39 -0500

    Fixed video playing in build

---
+++ WorldBlocks/Decor/video_player.gd
@@ -0,0 +1,8 @@
+extends MeshInstance3D
+
+var video = preload("res://Video/HomingAttack.ogv")
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	$SubViewportContainer/SubViewport/VideoStreamPlayer.stream = video
+
+
