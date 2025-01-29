be602c2b4b835c4e047dccf6a93c2026758842b3
Seanmus <seanpiche718@gmail.com>
2025-01-28 21:47:20 -0600

    updated level to be more fluid by decreasing jump lengths slightly and introducing barriers to make level easier

--- WorldBlocks/spawn_point.gd
+++ WorldBlocks/spawn_point.gd
@@ -1,12 +1,14 @@
 extends Area3D
 
+var hasBeenTriggered = false
 
 func _ready():
 	$CSGMesh3D.visible = false
 	$CSGCylinder3D.visible = false
 
 func _on_body_entered(body):
-	if body is player:
+	if body is player and not hasBeenTriggered:
+		hasBeenTriggered = true
 		print("Setting spawnPoint")
 		var Player = body as player
 		Player._SetSpawnPoint(position, rotation)
