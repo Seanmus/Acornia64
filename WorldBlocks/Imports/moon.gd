762ca4103e3528ea6109c9aed6092ea8123c7801
Seanmus <seanpiche718@gmail.com>
2025-03-10 13:53:11 -0500

    Changed aspect ratio to 16 x 9 and resized ui to fit, also updated title screen to be more animated

--- WorldBlocks/Imports/moon.gd
+++ WorldBlocks/Imports/moon.gd
@@ -3,4 +3,4 @@ extends Node3D
 @export var rotationRate: float = 0.5
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotate_x(rotationRate * delta)
+	rotate_y(rotationRate * delta)
