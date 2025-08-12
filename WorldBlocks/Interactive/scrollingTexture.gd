ae282423ef4852928b919fc33f4f67c6713c4bf7
Seanmus <seanpiche718@gmail.com>
2025-08-11 22:38:34 -0500

    Added scrolling texture for speed boost pads and made speed boost ramps launch player into the air

---
+++ WorldBlocks/Interactive/scrollingTexture.gd
@@ -0,0 +1,9 @@
+extends MeshInstance3D
+
+
+var speed  = 0.5
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	get_active_material(0).uv1_offset.y += delta * speed
+	#get_surface_override_material(0).uv1_offset.x = 1
+	
