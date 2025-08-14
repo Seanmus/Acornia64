8f65affdc90f1b0036578e76599700d64eabade7
Seanmus <seanpiche718@gmail.com>
2025-08-14 13:25:51 -0500

    Made speed boosts only work in intended direction

--- WorldBlocks/Interactive/speed_boost.gd
+++ WorldBlocks/Interactive/speed_boost.gd
@@ -5,5 +5,9 @@ extends Area3D
 #Increases the players new max speed
 func _on_speed_boost_body_entered(body):
 	if body.is_in_group("Player"):
+		print("Rotation" + str(global_rotation))
+		#body.velocity = direction * newMaxSpeed
 		print("Speed boost entered")
 		body._SpeedBoost(newMaxSpeed)
+		body.velocity.x = cos(global_rotation.y) * 50
+		body.velocity.z  = sin(global_rotation.y) * -50
