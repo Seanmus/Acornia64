a14b4ae61c5a41bade302559c37a7d82267be9ea
Seanmus <seanpiche718@gmail.com>
2025-08-20 17:22:52 -0500

    Disabled unused debugs for speed boost

--- WorldBlocks/Interactive/speed_boost.gd
+++ WorldBlocks/Interactive/speed_boost.gd
@@ -5,8 +5,6 @@ extends Area3D
 #Increases the players new max speed
 func _on_speed_boost_body_entered(body):
 	if body.is_in_group("Player"):
-		print("Rotation" + str(global_rotation))
-		print("Speed boost entered")
 		body._SpeedBoost(newMaxSpeed)
 		body.velocity.x = cos(global_rotation.y) * 50
 		body.velocity.z  = sin(global_rotation.y) * -50
