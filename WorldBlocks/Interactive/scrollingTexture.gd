6f3e698965085b1abbcb1a64111cf6c798f677b8
Seanmus <seanpiche718@gmail.com>
2025-08-23 22:59:58 -0500

    Fixed texture scrolling bug on speed boosts by only scrolling the material resource instead of each material instance, also improves performance.

--- WorldBlocks/Interactive/scrollingTexture.gd
+++ WorldBlocks/Interactive/scrollingTexture.gd
@@ -1,9 +1,10 @@
-extends MeshInstance3D
+extends Node
 
 
-var speed  = 0.5
+@export var speed  = 0.5
+@export var speedBoostMaterial : Material
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	get_active_material(0).uv1_offset.y += delta * speed
+	speedBoostMaterial.uv1_offset.y += delta * speed
 	#get_surface_override_material(0).uv1_offset.x = 1
 	
