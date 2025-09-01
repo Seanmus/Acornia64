f07e325fc52924e89e264bc8753c1e51213b26e5
Seanmus <seanpiche718@gmail.com>
2025-08-31 20:25:58 -0500

    Cleaned up file system a bit

--- TrapsAndEnemies/cannon.gd
+++ TrapsAndEnemies/cannon.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 #Preloads the cannonball asset so that it can be spawned in as many times as needed.
-@onready var seedShot = load("res://Traps/cannon_ball.tscn")
+@onready var seedShot = load("res://TrapsAndEnemies/cannon_ball.tscn")
 
 #Linked to the cannon object and shoots every time the timer runs out, afterwards the timer is restarted
 func _on_timer_timeout():
--- WorldBlocks/Interactive/start_cannon.gd
+++ WorldBlocks/Interactive/start_cannon.gd
@@ -3,7 +3,7 @@ extends Node3D
 var entered = false
 
 #Preloads the cannonball asset so that it can be spawned in as many times as needed.
-@onready var seedShot = load("res://Traps/auri_cannonBall.tscn")
+@onready var seedShot = load("res://TrapsAndEnemies/auri_cannonBall.tscn")
 @export var nextLevel : String
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player") && entered == false:
