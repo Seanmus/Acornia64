0da26ac5902ba64bd8f22c0715e58e568acca1a4
Seanmus <seanpiche718@gmail.com>
2025-09-03 22:13:36 -0500

    Fixed bug with speed boosts not allowing speeds above 50

--- WorldBlocks/Interactive/speed_boost.gd
+++ WorldBlocks/Interactive/speed_boost.gd
@@ -6,6 +6,6 @@ extends Area3D
 func _on_speed_boost_body_entered(body):
 	if body.is_in_group("Player"):
 		body._SpeedBoost(newMaxSpeed)
-		body.velocity.x = cos(global_rotation.y) * 50
-		body.velocity.z  = sin(global_rotation.y) * -50
+		body.velocity.x = cos(global_rotation.y) * newMaxSpeed
+		body.velocity.z  = sin(global_rotation.y) * -newMaxSpeed
 		body.velocity.y = -1000
