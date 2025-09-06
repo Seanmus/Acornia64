2a2368b2ae2328430b68e23b305103b36ad41a34
Seanmus <seanpiche718@gmail.com>
2025-09-05 23:43:34 -0500

    Polished the small section of city_level2 that is built

--- WorldBlocks/MovingPlatforms/moving_platform.gd
+++ WorldBlocks/MovingPlatforms/moving_platform.gd
@@ -12,6 +12,6 @@ func _ready():
 
 func move_tween():
 	var tween = get_tree().create_tween().set_loops()
-	tween.tween_property(path_follow3D, "progress_ratio" , 1.0, 5).set_ease(ease).set_trans(transition)
-	tween.tween_property(path_follow3D, "progress_ratio" , 0, 5).set_ease(ease).set_trans(transition)
+	tween.tween_property(path_follow3D, "progress_ratio" , 1.0, 3).set_ease(ease).set_trans(transition)
+	tween.tween_property(path_follow3D, "progress_ratio" , 0, 3).set_ease(ease).set_trans(transition)
 	tween.bind_node(self)
