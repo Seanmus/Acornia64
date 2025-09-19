12cd15cc3f0c517efcebd3d8ddaa8e3a45d11266
Seanmus <seanpiche718@gmail.com>
2025-09-18 23:10:30 -0500

    Made cannon ball speed an export variable on cannon so it can be individually controlled

--- TrapsAndEnemies/cannon.gd
+++ TrapsAndEnemies/cannon.gd
@@ -2,13 +2,14 @@ extends Node3D
 
 #Preloads the cannonball asset so that it can be spawned in as many times as needed.
 @onready var seedShot = load("res://TrapsAndEnemies/cannon_ball.tscn")
-
+@export var cannonBallSpeed = 20
 #Linked to the cannon object and shoots every time the timer runs out, afterwards the timer is restarted
 func _on_timer_timeout():
 	var s = seedShot.instantiate()
 	get_tree().root.add_child(s)
 	s.global_position = $muzzle.global_position
 	s.global_rotation = $muzzle.global_rotation
+	s.speed = cannonBallSpeed
 	#s.scale = self.scale
 	s._shoot()
 	$Timer.start()
