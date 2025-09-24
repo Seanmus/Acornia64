93442c379d33b5f6dbdc44e6b3e2b3db875e1d7a
Seanmus <seanpiche718@gmail.com>
2025-09-23 20:36:06 -0500

    Fixed angle of cannon on squirell_cannon so that it hits player consistently

--- TrapsAndEnemies/Enemies/squirell.gd
+++ TrapsAndEnemies/Enemies/squirell.gd
@@ -2,5 +2,5 @@ extends Node3D
 
 @onready var player = get_tree().get_nodes_in_group("Player")
 func _process(_delta: float) -> void:
-	if !Manager.won and player && position.distance_to(player[0].position) <= 100:
+	if !Manager.won and player:
 		look_at(player[0].global_position)
