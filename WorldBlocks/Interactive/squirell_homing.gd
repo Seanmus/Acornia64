2b7ba06ec409ffb6f7913eb00fadf6fdd8528671
Seanmus <seanpiche718@gmail.com>
2025-09-24 23:07:56 -0500

    Homing squirells only attack player if in 'sight'

--- WorldBlocks/Interactive/squirell_homing.gd
+++ WorldBlocks/Interactive/squirell_homing.gd
@@ -4,14 +4,25 @@ extends homingAttackDestroy
 @onready var startPos = position
 
 func _process(delta: float) -> void:
+
 	if !Manager.won and player && position.distance_to(player[0].position) <= 150:
-		position = position.move_toward(Vector3(player[0].position.x, player[0].position.y + 1, player[0].position.z), 10 * delta)
-		$squirell.look_at(player[0].position)
+		$RayCast3D.target_position = player[0].global_position - $RayCast3D.global_position
+		print($RayCast3D.get_collider())
+		if $RayCast3D.get_collider() != player[0]:
+			_ReturnToStart(delta)
+		else:
+			position = position.move_toward(Vector3(player[0].position.x, player[0].position.y + 1, player[0].position.z), 10 * delta)
+			$squirell.look_at(player[0].position)
 	else:
-		if position != startPos:
-			position = position.move_toward(startPos, 10 * delta)
-			if position.distance_to(startPos) > 10:
-				$squirell.look_at(startPos)
+		_ReturnToStart(delta)
+
+func _ReturnToStart(delta):
+	if position != startPos:
+		position = position.move_toward(startPos, 10 * delta)
+		if position.distance_to(startPos) > 10:
+			$squirell.look_at(startPos)
+
+
 func _Respawn():	
 	position = startPos
 	super._Respawn()
