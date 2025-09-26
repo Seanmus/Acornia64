5a1e820dde32a6625e80980e025fe0188293f112
Seanmus <seanpiche718@gmail.com>
2025-09-25 22:45:05 -0500

    Homing squirells will no longer overlapp one another

--- WorldBlocks/Interactive/squirell_homing.gd
+++ WorldBlocks/Interactive/squirell_homing.gd
@@ -1,11 +1,15 @@
 extends homingAttackDestroy
-
+class_name Squirell_Homing
 @onready var player = get_tree().get_nodes_in_group("Player")
 @onready var startPos = position
+var otherHoming = [self]
 
 func _process(delta: float) -> void:
+	if otherHoming.size() > 1:
+		position += (otherHoming[1].position - position) * -0.5 * delta
 
 	if !Manager.won and player && position.distance_to(player[0].position) <= 150:
+		print(otherHoming.size())
 		$RayCast3D.target_position = player[0].global_position - $RayCast3D.global_position
 		print($RayCast3D.get_collider())
 		if $RayCast3D.get_collider() != player[0]:
@@ -26,3 +30,13 @@ func _ReturnToStart(delta):
 func _Respawn():	
 	position = startPos
 	super._Respawn()
+
+
+func _on_area_entered(area: Area3D) -> void:
+	if area.is_in_group("enemies"):
+		otherHoming.append(area)
+
+
+func _on_area_exited(area: Area3D) -> void:
+	if area.is_in_group("enemies"):
+		otherHoming.erase(area)
