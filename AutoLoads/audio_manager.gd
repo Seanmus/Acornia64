61929cd2150b3fbdcc85c9f4cbfe826d943929b1
Sean Piche <seanpiche718@gmail.com>
2026-08-11 21:36:45 -0500

    Re enabled audio for bounce and added sound effect for speed boost

--- AutoLoads/audio_manager.gd
+++ AutoLoads/audio_manager.gd
@@ -2,9 +2,20 @@ extends Node
 
 @onready var seedCollectSound = get_node("seedCollect")
 @onready var coinCollectSound = get_node("coinCollect")
+@onready var Woosh = $Woosh
+@onready var startingPitch = $Woosh.pitch_scale
+@onready var startingDB = $Woosh.volume_db
 
 func _collectSeed():
 	seedCollectSound.play()
 
 func _collectCoin():
 	coinCollectSound.play()
+
+func _speedBoost():
+	if !Woosh.playing:
+		Woosh.pitch_scale = startingPitch
+		Woosh.volume_db = startingDB
+		Woosh.pitch_scale += randf_range(-0.05, 0.05)
+		Woosh.volume_db += randf_range(-0.5, 0.5)
+		Woosh.play()
--- WorldBlocks/Interactive/speed_boost.gd
+++ WorldBlocks/Interactive/speed_boost.gd
@@ -1,7 +1,6 @@
 extends Area3D
 
 @export var newMaxSpeed = 50
-
 #Increases the players new max speed
 func _on_speed_boost_body_entered(body):
 	if body.is_in_group("Player"):
@@ -9,3 +8,4 @@ func _on_speed_boost_body_entered(body):
 		body.velocity.x = cos(global_rotation.y) * newMaxSpeed
 		body.velocity.z  = sin(global_rotation.y) * -newMaxSpeed
 		body.velocity.y = -1000
+		AudioManager._speedBoost()
