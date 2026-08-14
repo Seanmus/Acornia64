923f3c54412605b50588725403edcb16b4cc642d
Sean Piche <seanpiche718@gmail.com>
2026-08-13 21:35:17 -0500

    Fixed how outline works with focus on level select

--- Worlds/levelSelectButton.gd
+++ Worlds/levelSelectButton.gd
@@ -1,13 +1,11 @@
 extends TextureButton
 
 
-@onready var outline = $Outline
-
 
 func _on_focus_entered() -> void:
 	grab_focus()
 	print("focused")
-	$Outline.visible = true
+
 
 func _on_focus_exited() -> void:
-	$Outline.visible = false
+	release_focus()
