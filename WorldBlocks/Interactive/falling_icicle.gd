fd37d8fac1675e275b6c97e63e301ebc028bc71d
Sean Piche <seanpiche718@gmail.com>
2026-08-19 21:57:09 -0500

    Added droping icicle

--- WorldBlocks/Interactive/falling_icicle.gd
+++ WorldBlocks/Interactive/falling_icicle.gd
@@ -1,9 +1,30 @@
 extends Node3D
 
+var shaking = false
+var falling = false
+@onready var startPos = position
+
 
 func _on_trigger_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		$AnimationPlayer.play("icicle_fall")
+		shaking = true
+		$Timer.start()
 
-func _delete_icicle():
-	queue_free()
+
+func _physics_process(delta: float) -> void:
+	if falling:
+		position.y -= 2
+	elif shaking:
+		position.x += randf_range(-0.5, 0.5)
+		position.z += randf_range(-0.5, 0.5)
+	
+
+func _on_timer_timeout() -> void:
+	shaking = false
+	falling = true
+
+func _Respawn():
+	falling = false
+	shaking = false
+	$Timer.stop()
+	position = startPos
